5f426699c765832c63f8e42bc0c8dfeea7c4c67b
Jesus-Almansa <jtafernandez@gmail.com>
2024-10-07 13:09:45 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LLM',
+    version='0.1.0',
+    packages=find_packages(),
+  author='Jesús Almansa',
+    description='LLM project'
+)
+
